56a11c0d1469d1266e34cd56037d103d7fa7fee3
Izzan <muhammadizzanaquilla@gmail.com>
2025-12-19 17:14:17 +0700

    Maze

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
-import ui.MazeFrame;
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2025/2026
+ * Ladder Game
+ * Muhammad Izzan Aquilla - 5026241069
+ * Muhammad Faqih Maulana - 5026241174
+ */
 
+import ui.MazeFrame;
 import javax.swing.SwingUtilities;
 
 public class Main {
